40edc864663ab539b728970abc5c6a6f011c6ed2
jacobponce <ponce.jacob.t@gmail.com>
2024-07-14 17:18:00 -0700

    added new location

--- app/(tabs)/EventCalendar/EventForm/NewEvent.tsx
+++ app/(tabs)/EventCalendar/EventForm/NewEvent.tsx
@@ -7,7 +7,7 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 
 const locationsDictionary: { [key: string]: { latitude: number; longitude: number } } = {
   "Business": { latitude:35.30015127563236, longitude:-120.66523106002076 },
-  "Location B": { latitude: 34.052235, longitude: -118.243683 },
+  "Jacob House": { latitude: 35.30816, longitude: -120.65937 },
   "Location C": { latitude: 37.774929, longitude: -122.419418 },
   // Add more locations as needed
 };
